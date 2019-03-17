fa9e272bd094bf8499ed9305e55e37764fb546df
apple <13671653851@163.com>
2019-03-17 16:22:21 +0800

    Initial commit

---
+++ venv/Scripts/get_environment-script.py
@@ -0,0 +1,12 @@
+#!C:\ZWJ\code\test_matminer\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pymatgen==2019.2.28','console_scripts','get_environment'
+__requires__ = 'pymatgen==2019.2.28'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pymatgen==2019.2.28', 'console_scripts', 'get_environment')()
+    )
