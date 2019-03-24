e4fedc45b189f671c44312d009c9d1e70cc1cfcf
apple <13671653851@163.com>
2019-03-24 21:53:13 +0800

    按照示例实验一个随机森林模型

--- basic_test_ml.py
+++ basic_test_ml.py
@@ -10,82 +10,84 @@ import pickle as pkl
 from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score, cross_val_predict, GridSearchCV, ShuffleSplit, KFold
-import  multiprocessing
+import multiprocessing
 
 quick_demo = True
 
 mdf = retrieve_MDF.MDFDataRetrieval(anonymous=True)
 if __name__ == '__main__':
     # 得到数据
-    query_string = 'mdf.source_name:oqmd AND (oqmd.configuration:static OR '\
-        'oqmd.configuration:standard) AND dft.converged:True'
+    query_string = 'mdf.source_name:oqmd AND (oqmd.configuration:static OR ' \
+                   'oqmd.configuration:standard) AND dft.converged:True'
     if quick_demo:
         query_string += " AND mdf.scroll_id:<10000"
 
     data = mdf.get_data(query_string, unwind_arrays=False)
     print(data.head())
-    # 重命名、预处理和筛选
+    # 重命名、预处理和筛选,delta_e应该是形成能
     data = data[['oqmd.delta_e.value', 'material.composition']]
-    data = data.rename(columns={'oqmd.delta_e.value': 'delta_e', 'material.composition':'composition'})
+    data = data.rename(columns={'oqmd.delta_e.value': 'delta_e', 'material.composition': 'composition'})
     data = StrToComposition(target_col_id='composition_obj').featurize_dataframe(data, 'composition')
+    print(data.head(3))
     for k in ['delta_e']:
         data[k] = pd.to_numeric(data[k])
 
     original_count = len(data)
     data = data[~ data['delta_e'].isnull()]
-    print('Removed %d/%d entries'%(original_count - len(data), original_count))
+    print('Removed %d/%d entries' % (original_count - len(data), original_count))
 
     original_count = len(data)
     data['composition'] = data['composition_obj'].apply(lambda x: x.reduced_formula)
     data.sort_values('delta_e', ascending=True, inplace=True)
     data.drop_duplicates('composition', keep='first', inplace=True)
-    print('Removed %d/%d entries'%(original_count - len(data), original_count))
-
+    print('Removed %d/%d entries' % (original_count - len(data), original_count))
+    # 用逻辑和的方式筛选[-20,5]范围内的delta_e
     original_count = len(data)
     data = data[np.logical_and(data['delta_e'] >= -20, data['delta_e'] <= 5)]
-    print('Removed %d/%d entries'%(original_count - len(data), original_count))
+    print('Removed %d/%d entries' % (original_count - len(data), original_count))
 
-    #建立机器学习模型
+    # 设定化学计算规范：使用MagpieData数据源初始化元素属性，返回各层轨道电子数量信息，假设元素以单一氧化态存在
     feature_calculators = MultipleFeaturizer([cf.Stoichiometry(), cf.ElementProperty.from_preset("magpie"),
-                                              cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True)])
-    #获得特征名
+                                              cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=False)])
+    # 获得特征名
     feature_labels = feature_calculators.feature_labels()
-    #计算特征量
+    print(feature_labels[0:5:])
+    # 计算特征量
     data = feature_calculators.featurize_dataframe(data, col_id='composition_obj');
-    print('Generated %d features'%len(feature_labels))
+    print('Generated %d features' % len(feature_labels))
     print('Training set size:', 'x'.join([str(x) for x in data[feature_labels].shape]))
-    #去除空值缺省值
+    # 去除空值缺省值
     original_count = len(data)
     data = data[~ data[feature_labels].isnull().any(axis=1)]
-    print('Removed %d/%d entries'%(original_count - len(data), original_count))
+    print('Removed %d/%d entries' % (original_count - len(data), original_count))
     # 调用随机森林
     # “随机森林”算法通过训练许多不同的决策树模型来工作，
     # 其中每个模型都在数据集的不同子集上进行训练。
     # 此处，调整的主要参数是每棵树考虑的特征个数
     model = GridSearchCV(RandomForestRegressor(n_estimators=20 if quick_demo else 150, n_jobs=-1),
-                         param_grid=dict(max_features=range(8,15)),
-                         scoring='neg_mean_squared_error',cv=ShuffleSplit(n_splits=1, test_size=0.1))
+                         param_grid=dict(max_features=range(8, 15)),
+                         scoring='neg_mean_squared_error', cv=ShuffleSplit(n_splits=1, test_size=0.1))
     model.fit(data[feature_labels], data['delta_e'])
     # 找到的最佳特征集
-    print(model.best_score_)
-    #画出最大特征量与准确度的关系
+    print("model.best_score:"+model.best_score_)
+    # 画出最大特征量与准确度的关系
     fig, ax = plt.subplots()
     ax.scatter(model.cv_results_['param_max_features'].data,
-              np.sqrt(-1 * model.cv_results_['mean_test_score']))
-    ax.scatter([model.best_params_['max_features']], np.sqrt([-1*model.best_score_]), marker='o', color='r', s=40)
+               np.sqrt(-1 * model.cv_results_['mean_test_score']))
+    ax.scatter([model.best_params_['max_features']], np.sqrt([-1 * model.best_score_]), marker='o', color='r', s=40)
     ax.set_xlabel('Max. Features')
     ax.set_ylabel('RMSE (eV/atom)')
-    #保存最佳模型
+    # 保存最佳模型
     model = model.best_estimator_
-    #10次交叉验证
+    # 10次交叉验证
     cv_prediction = cross_val_predict(model, data[feature_labels], data['delta_e'], cv=KFold(10, shuffle=True))
-    #计算汇总统计数据
+    # 计算汇总统计数据
     for scorer in ['r2_score', 'mean_absolute_error', 'mean_squared_error']:
-        score = getattr(metrics,scorer)(data['delta_e'], cv_prediction)
+        score = getattr(metrics, scorer)(data['delta_e'], cv_prediction)
         print(scorer, score)
 
     print(model)
-    #绘制每个预测
+    # 绘制每个预测
     fig, ax = plt.subplots()
 
     ax.hist2d(pd.to_numeric(data['delta_e']), cv_prediction, norm=LogNorm(), bins=64, cmap='Blues', alpha=0.9)
@@ -97,7 +99,7 @@ if __name__ == '__main__':
     r2 = metrics.r2_score(data['delta_e'], cv_prediction)
     ax.text(0.5, 0.1, 'MAE: {:.2f} eV/atom\n$R^2$:  {:.2f}'.format(mae, r2),
             transform=ax.transAxes,
-           bbox={'facecolor': 'w', 'edgecolor': 'k'})
+            bbox={'facecolor': 'w', 'edgecolor': 'k'})
 
     ax.plot(ax.get_xlim(), ax.get_xlim(), 'k--')
 
